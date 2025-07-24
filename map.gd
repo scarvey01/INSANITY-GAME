1c8f911711ebe50339496ea391bc36210b85668c
Kendrick Fithen <fithenkendrick@gmail.com>
2025-07-24 16:25:33 -0400

    Added a second cactus
    - Added a second cactus model
    - Allowed second cactus to be generated on the map
    - Fixed cactus textures

--- map.gd
+++ map.gd
@@ -1,12 +1,15 @@
 extends StaticBody3D
 
-var cactusScene = preload("res://cactus.tscn")
+var cactus_1 = preload("res://cactus1.tscn")
+var cactus_2 = preload("res://cactus2.tscn")
+
+var cacti = [cactus_1, cactus_2]
 
 func spawnCacti() -> void:
 	for x in range(-45, 45, 5):
 		for y in range(-45, 45, 5):
 			if randi_range(1, 10) == 1:
-				var cactusInstance = cactusScene.instantiate()
+				var cactusInstance = cacti[randi_range(0, 1)].instantiate()
 				cactusInstance.position = Vector3(x, 0, y)
 				cactusInstance.rotation_degrees = Vector3(0, randf_range(0, 360), 0)
 				add_child(cactusInstance)
