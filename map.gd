a130730bb2ca1adc331746637e0a9f0f082aa7f8
Kendrick Fithen <124932921+kfithen@users.noreply.github.com>
2025-08-02 02:26:02 -0400

    Added a 3rd cactus
    - Added a 3rd cactus model
    - Added the 3rd cactus into map generation
    - Changed renderer to gl_compatibility so my shit laptop can run the game

--- map.gd
+++ map.gd
@@ -2,14 +2,15 @@ extends StaticBody3D
 
 var cactus_1 = preload("res://cactus1.tscn")
 var cactus_2 = preload("res://cactus2.tscn")
+var cactus_3 = preload("res://cactus3.tscn")
 
-var cacti = [cactus_1, cactus_2]
+var cacti = [cactus_1, cactus_2, cactus_3]
 
 func spawnCacti() -> void:
 	for x in range(-45, 45, 5):
 		for y in range(-45, 45, 5):
 			if randi_range(1, 10) == 1:
-				var cactusInstance = cacti[randi_range(0, 1)].instantiate()
+				var cactusInstance = cacti[randi_range(0, len(cacti) - 1)].instantiate()
 				cactusInstance.position = Vector3(x, 0, y)
 				cactusInstance.rotation_degrees = Vector3(0, randf_range(0, 360), 0)
 				add_child(cactusInstance)
